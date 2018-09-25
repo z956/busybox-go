5071c4cd672681a472fc744754b240d7a653db45
z956 <z789512346@gmail.com>
2018-09-25 22:03:59 +0800

    add getopt and parsing head command line

---
+++ getopt.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const OPT_NO_ARG = 0
+const OPT_REQUIRE_ARG = 1
+
+type Option struct {
+	OptLong  string
+	OptShort byte
+	HasArg   int
+}
+
+func (o Option) hasLong() bool {
+	return o.OptLong != ""
+}
+func (o Option) hasShort() bool {
+	return o.OptShort != 0
+}
+
+type ParseResultFunc func(optlong string, optshort byte, val string, userdata interface{}) error
+
+func OptParse(args []string, opts []Option, cb ParseResultFunc, userdata interface{}) error {
+	if err := checkOpts(opts); err != nil {
+		return err
+	}
+
+	arglen := len(args)
+	for i := 0; i < arglen; i++ {
+		ss := strings.SplitN(args[i], "=", 2)
+
+		if r, err := parseArg(opts, ss[0]); err != nil {
+			return err
+		} else {
+			if len(r) == 0 {
+				//not an option
+				if err := cb("", 0, args[i], userdata); err != nil {
+					return err
+				}
+			} else {
+				for _, o := range r {
+					if o.HasArg == OPT_NO_ARG {
+						if err := cb(o.OptLong, o.OptShort, "", userdata); err != nil {
+							return err
+						}
+					} else if o.HasArg == OPT_REQUIRE_ARG {
+						if len(ss) == 2 {
+							if err := cb(o.OptLong, o.OptShort, ss[1], userdata); err != nil {
+								return err
+							}
+						} else if i == arglen-1 {
+							return fmt.Errorf("Missing argument with option '%s'", ss[0])
+						} else {
+							i += 1
+							if err := cb(o.OptLong, o.OptShort, args[i], userdata); err != nil {
+								return err
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func checkOpts(opts []Option) error {
+	for i, o := range opts {
+		if !o.hasLong() && !o.hasShort() {
+			return fmt.Errorf("Empty option")
+		}
+		if o.hasShort() && !isOptValid(o.OptShort) {
+			return fmt.Errorf("Invalid short opt: %x", o.OptShort)
+		}
+		if o.HasArg != OPT_NO_ARG && o.HasArg != OPT_REQUIRE_ARG {
+			return fmt.Errorf("Invalid opt flag: %d", o.HasArg)
+		}
+
+		for _, oo := range opts[0:i] {
+			if oo.hasLong() && o.hasLong() && oo.OptLong == o.OptLong {
+				return fmt.Errorf("Duplicate long opt: %s", o.OptLong)
+			}
+			if oo.hasShort() && o.hasShort() && oo.OptShort == o.OptShort {
+				return fmt.Errorf("Duplicate short opt: %c", o.OptShort)
+			}
+		}
+	}
+	return nil
+}
+
+func parseArg(opts []Option, arg string) ([]Option, error) {
+	//arg should be strip with '='
+	result := make([]Option, 0)
+	if strings.HasPrefix(arg, "--") && arg != "--" {
+		//long opt
+		arg = arg[2:]
+		if o, err := getOptionByLong(opts, arg); err != nil {
+			return nil, err
+		} else {
+			result = append(result, o)
+		}
+	} else if strings.HasPrefix(arg, "-") && arg != "-" {
+		//short opts, may be aggregated
+		//only the last opt can has arg
+		arg = arg[1:]
+		arglen := len(arg)
+
+		for i := 0; i < arglen; i++ {
+			if o, err := getOptionByShort(opts, arg[i]); err != nil {
+				return nil, err
+			} else {
+				if i != arglen-1 && o.HasArg == OPT_REQUIRE_ARG {
+					return nil, fmt.Errorf("Invalid argument with opt '%c'", arg[i])
+				}
+				result = append(result, o)
+			}
+		}
+	}
+
+	return result, nil
+}
+
+func getOptionByLong(opts []Option, opt string) (Option, error) {
+	for _, o := range opts {
+		if o.hasLong() && opt == o.OptLong {
+			return o, nil
+		}
+	}
+	return Option{}, fmt.Errorf("Unknown option: %s", opt)
+}
+
+func getOptionByShort(opts []Option, opt byte) (Option, error) {
+	for _, o := range opts {
+		if o.hasShort() && opt == o.OptShort {
+			return o, nil
+		}
+	}
+	return Option{}, fmt.Errorf("Unknown option: %s", opt)
+}
+
+func isOptValid(b byte) bool {
+	switch {
+	case b >= 'a' && b <= 'z':
+		return true
+	case b >= 'A' && b <= 'Z':
+		return true
+	case b >= '0' && b <= '9':
+		return true
+	default:
+		return false
+	}
+}
--- head.go
+++ head.go
@@ -1,11 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
-type Head struct{}
+type Head struct {
+	file    string
+	isStdin bool
+	num     int
+
+	isLineMode bool
+	isVerbose  bool
+}
+
+var headOpts = initOpt()
+
+func initOpt() []Option {
+	return []Option{
+		{"", 'n', OPT_REQUIRE_ARG},
+		{"", 'c', OPT_REQUIRE_ARG},
+		{"", 'v', OPT_NO_ARG},
+		{"", 'q', OPT_NO_ARG},
+	}
+}
+
+func parseSizeOpt(val string) (int, error) {
+	//val: 10
+	//val: 10k, 10K (1024)
+	//val: 10b, 10B (512)
+	//val: 10m, 10M (1024 * 1024)
+	//others are invalid
+	last := val[len(val)-1]
+	mul := 1
+	switch last {
+	case 'k', 'K':
+		val = val[:len(val)-1]
+		mul = 1024
+	case 'b', 'B':
+		val = val[:len(val)-1]
+		mul = 512
+	case 'm', 'M':
+		val = val[:len(val)-1]
+		mul = 1024 * 1024
+	default:
+	}
+
+	if size, err := strconv.Atoi(val); err != nil {
+		return 0, fmt.Errorf("Invalid size value: %s", val)
+	} else {
+		size *= mul
+		return size, nil
+	}
+}
+func parseResult(optlong string, optshort byte, val string, userdata interface{}) error {
+	h := userdata.(*Head)
+	var err error
+	switch optshort {
+	case 'n':
+		if h.num, err = parseSizeOpt(val); err != nil {
+			return err
+		}
+		h.isLineMode = true
+	case 'c':
+		if h.num, err = parseSizeOpt(val); err != nil {
+			return err
+		}
+		h.isLineMode = false
+	case 'v':
+		h.isVerbose = true
+	case 'q':
+		h.isVerbose = false
+	default:
+		//file
+		if _, err := os.Stat(val); os.IsNotExist(err) {
+			return err
+		} else {
+			h.file = val
+			h.isStdin = false
+		}
+	}
+	return nil
+}
 
 func NewHead(args []string) (Command, error) {
-	return nil, fmt.Errorf("head is not implement yet\n")
+	h := Head{"", true, 10, true, false}
+
+	if err := OptParse(args[1:], headOpts, parseResult, &h); err != nil {
+		return nil, err
+	}
+
+	return h, nil
 }
 
 func UsageHead() {
@@ -24,6 +110,6 @@ func UsageHead() {
 }
 
 func (h Head) Run() int {
-	fmt.Println("head running")
+	fmt.Printf("head running, head: %v\n", h)
 	return 0
 }
